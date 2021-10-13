c4f481e13021dc96809e14a57387ba90924640fc
DeveshSood <77494434+DeveshSood@users.noreply.github.com>
2021-10-13 19:09:00 +0530

    Update Covid.py

--- Project 8/Covid.py
+++ Project 8/Covid.py
@@ -3,6 +3,6 @@ import plotly.express as px
 
 df = pd.read_csv("Covid.csv")
 
-fig=px.scatter(df, x="Date", y="Cases", color="Country", title="C")
+fig=px.scatter(df, x="Date", y="Cases", color="Country", title="Covid Report")
 
 fig.show()
